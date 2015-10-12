195665655a16286970b6a816bd57cd05f599de96
Jon Burgess <jkburges@gmail.com>
2015-10-12 13:35:27 +1100

    Chained processes

--- src/main/java/au/org/emii/wps/NotifierProcess.java
+++ src/main/java/au/org/emii/wps/NotifierProcess.java
@@ -3,6 +3,7 @@ package au.org.emii.wps;
 import org.geotools.process.ProcessException;
 import org.geoserver.wps.gs.GeoServerProcess;
 import org.geoserver.wps.process.RawData;
+import org.geotools.process.factory.DescribeParameter;
 import org.geotools.process.factory.DescribeProcess;
 import org.geotools.process.factory.DescribeResult;
 
@@ -18,27 +19,10 @@ public class NotifierProcess implements GeoServerProcess {
 
     @DescribeResult(name="result", description="NetCDF file", meta={"mimeTypes=application/x-netcdf"})
     public RawData execute(
-        // @DescribeParameter(name="layer", description="WFS layer to query")
-        // String layer,
-        // @DescribeParameter(name="subset", description="Subset, semi-colon separated")
-        // String subset,
-        // ProgressListener progressListener
+        @DescribeParameter(name="notifiable", description="NetCDF file")
+        RawData notifiableData
         ) throws ProcessException {
 
-        logger.error("Hello world");
-
-        return new RawData() {
-            public String getMimeType() {
-                return "text/plain";
-            }
-
-            public InputStream getInputStream() throws IOException {
-                return new ByteArrayInputStream("wps result FTW".getBytes(java.nio.charset.StandardCharsets.UTF_8));
-            }
-
-            public String getFileExtension() {
-                return "txt";
-            }
-        };
+        return notifiableData;
     }
 }
